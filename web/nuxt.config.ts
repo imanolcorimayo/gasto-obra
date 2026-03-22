f5c7ee3d7d3a84a7707ff309307133346b348afc
Imanol <imanol@meowtel.com>
2026-03-22 16:03:57 -0300

    minor fix-auto zoom iphone

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -62,7 +62,7 @@ export default defineNuxtConfig({
       meta: [
         {
           name: "viewport",
-          content: "width=device-width, initial-scale=1.0"
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0"
         }
       ]
     }
